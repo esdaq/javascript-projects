308b835b27df4e8fe6e473b2ed0f46fee15e1ff2
d-esda <onedesda@gmail.com>
2024-06-14 00:13:47 -0400

    finished chapter 4 excercises

--- data-and-variables/exercises/data-and-variables-exercises.js
+++ data-and-variables/exercises/data-and-variables-exercises.js
@@ -1,11 +1,34 @@
 // Declare and assign the variables below
 
+let spaceShuttleName = "Determination";
+let shuttleSpeedMph = 17500;
+let kilometersToMars = 225000000;
+let kilometersToMoon = 384000;
+let milesPerKilometer = 0.621;
+
 // Use console.log to print the 'typeof' each variable. Print one item per line.
+
+console.log(typeof spaceShuttleName);
+console.log(typeof shuttleSpeedMph);
+console.log(typeof kilometersToMars);
+console.log(typeof kilometersToMoon);
+console.log(typeof milesPerKilometer);
 
 // Calculate a space mission below
 
+let milesToMars = kilometersToMars * milesPerKilometer;
+let hoursToMars = milesToMars / shuttleSpeedMph;
+let daysToMars = hoursToMars / 24;
 // Print the results of the space mission calculations below
+
+console.log(spaceShuttleName + " will take " + daysToMars + " days to reach Mars.");
 
 // Calculate a trip to the moon below
 
+let milesToMoon = kilometersToMoon * milesPerKilometer;
+let hoursToMoon = milesToMoon / shuttleSpeedMph;
+let daysToMoon = hoursToMoon / 24;
+
 // Print the results of the trip to the moon below
+
+console.log(spaceShuttleName + " will take " + daysToMoon + " days to reach the Moon.");
--- how-to-write-code/consolelogexamples01.js
+++ how-to-write-code/consolelogexamples01.js
@@ -4,3 +4,10 @@ console.log("What","do","commas","do?");
 console.log("Does", "adding",      "space", "matter?");
 console.log('Launch' + 'Code');
 console.log("LaunchCode was founded in", 2013);
+console.log("Bruce's beard");
+console.log(Number("three"));
+
+const input = require('readline-sync');
+
+let name = input.question("Enter your name: ");
+console.log("Hello ", name);
