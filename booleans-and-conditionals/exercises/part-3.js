7778939dc63459a0dc8f9135d72ea76782b0bb5c
d-esda <onedesda@gmail.com>
2024-06-18 11:25:05 -0400

    added my solution to other problem parts

--- booleans-and-conditionals/exercises/part-3.js
+++ booleans-and-conditionals/exercises/part-3.js
@@ -16,9 +16,38 @@ e) If fuelLevel is below 1000 OR engineTemperature is above 3500 OR engineIndica
 
 f) Otherwise, print "Fuel and engine status pending..." */
 
+if((fuelLevel > 20_000) && engineTemperature <= 2500) {
+  console.log("Full tank. Engines good");
+} else if ((fuelLevel > 10_000) && engineTemperature <= 2500) {
+  console.log("Fuel level above 50%. Engines good");
+} else if ((fuelLevel > 5000) && engineTemperature <= 2500) {
+  console.log("Fuel level above 25%. Engines good");
+} else if (fuelLevel <= 50000 || engineTemperature > 2500) {
+  console.log("Check fuel level. Engines running hot.");
+} else if (fuelLevel < 1000 || engineTemperature > 3500
+  || engineIndicatorLight === "red blinking" ) {
+ console.log("ENGINE FAILURE IMMINENT!");
+} else {
+  console.log("Fuel and engine status pending…");
+}
+
 // Code 5a - 5f here:
 
 // 6) a) Create the variable commandOverride, and set it to be true or false. If commandOverride is false, then the shuttle should only launch if the fuel and engine check are OK. If commandOverride is true, then the shuttle will launch regardless of the fuel and engine status.
+let commandOverride = true;
 
+if (!commandOverride && fuelLevel > 5000
+  && engineTemperature <= 2500) {
+ console.log("Launch in sequence");
+} else if (commandOverride) {
+ console.log("Launch in sequence");
+}
 /* 6) b) Code the following if/else check:
 If fuelLevel is above 20000 AND engineIndicatorLight is NOT red blinking OR commandOverride is true print "Cleared to launch!" Else print "Launch scrubbed!" */
+
+if (fuelLevel > 20_000 && engineIndicatorLight
+  === "is NOT red blinking" || commandOverride) {
+ console.log("Cleared to launch");
+} else {
+ console.log("Launched scrubbed");
+}
