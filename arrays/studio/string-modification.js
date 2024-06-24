b56d909250bba8dd8c828d5cd21e958d99e33343
d-esda <onedesda@gmail.com>
2024-06-24 14:50:48 -0400

    refactored studio assign

--- arrays/studio/string-modification.js
+++ arrays/studio/string-modification.js
@@ -6,23 +6,32 @@ outputString = "";
 //1) Use string methods to remove the first three characters from the string and add them to the end.
 //Hint - define another variable to hold the new string or reassign the new string to str.
 
-// let str2 = str.slice(0, 3);
-// str2 = str.slice(3) + str2;
+let str2 = str.slice(0, 3);
+str2 = str.slice(3) + str2;
 
 //Use a template literal to print the original and modified string in a descriptive phrase.
 
 //2) Modify your code to accept user input. Query the user to enter the number of letters that will be relocated.
-let moveLength = input.question("Enter number: ");
-/*let str3 = str.slice(0, Number(relocateLength));
+let desc = `Program removes the first nth characters from ` +
+`string and appends the removed characters to end of the string`;
+
+console.log(desc + "\n");
+console.log("String: " + str);
+let moveLength = input.question("Enter number of chracters to move: ");
+/*let str3 = str.slice(0, Number(move));
 console.log(str3)
-str4 = str.slice(Number(relocateLength)) + str3;
-console.log(str4); */
+str4 = str.slice(Number(moveLength)) + str3;
+console.log(str4); */ 
 //3) Add validation to your code to deal with user inputs that are longer than the word. In such cases, default to moving 3 characters. Also, the template literal should note the error.
 
-if(str.length < moveLength) {
+if(moveLength > str.length ) {
   substring = str.slice(0, 3);
   outputString = str.slice(3) + substring;
   console.log(outputString);
-  console.log("Error: world length is too short defaulted to 3");
+  console.log("Error: world length is less than the character move length - defaulted to 3...");
+} else {
+  substring = str.slice(0, moveLength);
+  outputString = str.slice(moveLength) + substring;
+  console.log(outputString)
 }
 
