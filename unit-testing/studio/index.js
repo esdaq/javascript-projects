6aa98cc08fe0835ce57efac67d96296a2d0c6631
d-esda <onedesda@gmail.com>
2024-07-15 21:15:13 -0400

    finished unit testing studio

--- unit-testing/studio/index.js
+++ unit-testing/studio/index.js
@@ -1,7 +1,31 @@
 
 let launchcode = {
-  
-}
+  organization: "nonprofit",
+  executiveDirector: "Jeff",
+  percentageCoolEmployees: 100,
+  programsOffered: ["Web Development", "Data Analysis", "Liftoff"],
+  launchOutput : function (input) {
+    let output = "";
+
+    if (input % 2 === 0 && input % 3 === 0 && input % 5 === 0) {
+      return output = 'LaunchCode Rocks!';
+    }else if (input % 2 === 0 && input % 3 === 0) {
+      return output = "LaunchCode!";
+    }else if (input % 3 === 0 && input % 5 === 0) {
+      return output = "Code Rocks!";
+    } else if (input % 2 === 0 && input % 5 === 0) {
+      return output = 'Launch Rocks! (CRASH!!!!)';
+    }  else if (input % 2 === 0) {
+      return output = 'Launch!';
+    } else if (input % 3 === 0) {
+      return output = 'Code!';
+    } else if (input % 5 === 0) {
+      return output = 'Rocks!';
+    } else {
+      return output = "Rutabagas! That doesn't work.";
+    }
+  }
+};
 
 module.exports = launchcode;
 
--- unit-testing/studio/tests/launchcode.test.js
+++ unit-testing/studio/tests/launchcode.test.js
@@ -3,6 +3,37 @@ const launchcode = require('../index.js');
 
 describe("Testing launchcode", function(){
 
-  // Write your unit tests here!
+  test("organization should contain nonprofit", function() {
+    expect(launchcode.organization).toBe("nonprofit");
+  });
+
+  test("executive director should contain jeff", function() {
+    expect(launchcode.executiveDirector).toBe("Jeff");
+  });
+
+  test("percentageCoolEmployees should contain 100", function() {
+    expect(launchcode.percentageCoolEmployees).toBe(100);
+  });
+
+  test("programsOffered should contain ", function() {
+    expect(launchcode.programsOffered[0]).toBe("Web Development");
+    expect(launchcode.programsOffered[1]).toBe("Data Analysis");
+    expect(launchcode.programsOffered[2]).toBe("Liftoff");
+    expect(launchcode.programsOffered.length).toBe(3);
+  });
+
+
+  test("launchOutput() method meets output", function() {
+    expect(launchcode.launchOutput(2)).toBe("Launch!");
+    expect(launchcode.launchOutput(3)).toBe("Code!");
+    expect(launchcode.launchOutput(5)).toBe("Rocks!");
+    expect(launchcode.launchOutput(6)).toBe("LaunchCode!");
+    expect(launchcode.launchOutput(30)).toBe("LaunchCode Rocks!");
+    expect(launchcode.launchOutput(7)).toBe("Rutabagas! That doesn't work.");
+    expect(launchcode.launchOutput(15)).toBe("Code Rocks!");
+    expect(launchcode.launchOutput(10)).toBe("Launch Rocks! (CRASH!!!!)");
+
+  });
   
 });
+
